468d750b48eae3e401702b3d1516ccd8d8de7f81
Ritik Jain <ritik.jain@myntra.com>
2024-05-22 15:11:07 +0530

    setup

--- App.tsx
+++ App.tsx
@@ -6,23 +6,18 @@
  */
 
 import React from 'react'
-import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native'
+import {SafeAreaView, StatusBar, Text} from 'react-native'
 
 import {Colors} from 'react-native/Libraries/NewAppScreen'
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark' || true
-  console.log(isDarkMode)
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: Colors.lighter,
   }
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+      <StatusBar barStyle={'dark-content'} backgroundColor={Colors.dark} />
       <Text>Hello</Text>
     </SafeAreaView>
   )
