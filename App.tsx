099a7d100c7ae7a909afdfa78f8c07563fd0e98f
Ritik Jain <ritik.jain@myntra.com>
2024-05-25 00:08:20 +0530

    added types

--- App.tsx
+++ App.tsx
@@ -10,17 +10,15 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import React from 'react'
 
 import {routes} from './src/screens/routes/routeConfig'
-import {RouteParamsList} from './src/screens/routes/type'
+import {RouteConfiguration, RouteParamsList} from './src/screens/routes/type'
 
 const Stack = createNativeStackNavigator<RouteParamsList>()
 function App(): React.JSX.Element {
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown: false}}>
-                {routes.map(route => {
-                    // const route = _route as RouteConfiguration<
-                    //     typeof _route.routeName
-                    // >
+                {routes.map(_route => {
+                    const route = _route as RouteConfiguration<'home' | 'login'>
                     return (
                         <Stack.Screen
                             name={route.routeName}
